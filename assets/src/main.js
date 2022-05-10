497f7833d03cc059b3a9ff73d35553fe71af0562
arcadio <c4net.7@gmail.com>
2022-05-10 04:39:43 +0430

    render footer from js

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,7 +1,9 @@
 // import "core-js";
 import { navRender } from "./components/navRender.js";
+import { renderFooter } from "./components/footer";
 const windowLoad = () => {
   navRender();
+  renderFooter();
 };
 
 windowLoad();
